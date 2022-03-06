6fc1aa88153d5149ba54c9263ad806596afaeb21
roni3840 <roni3821@hotmail.com>
2022-03-06 16:21:54 +0200

    Init commit.

---
+++ app/src/main/java/com/example/workmanagertutorial/WorkerKeys.kt
@@ -0,0 +1,9 @@
+package com.example.workmanagertutorial
+
+object WorkerKeys {
+
+    const val ERROR_MSG = "errorMsg"
+    const val IMAGE_URI = "imageUri"
+    const val FILTER_URI = "filterUri"
+
+}
